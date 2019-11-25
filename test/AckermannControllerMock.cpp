98b07c8d17e628e36cb16a25586038d255d7a324
Gautam Balachandran <47335017+Gautam-Balachandran@users.noreply.github.com>
2019-11-24 20:42:23 -0500

    Updating AckermannControllerMock to account for cppLint errors

--- test/AckermannControllerMock.cpp
+++ test/AckermannControllerMock.cpp
@@ -1,3 +1,19 @@
+/*
+ * @file AckermannControllerMock.hpp
+ * @Author Gautam Balachandran
+ * Created on 24 November 2019
+ * @brief Ackermann Controller GMock test file
+ */
+
+/*
+ The MIT License
+ Copyright 2019 Gautam Balachandran, Sri Sai Kaushik, Sri Manika Makam
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #include <gmock/gmock.h>
 #include <vector>
 #include <AckermannController.h>
@@ -13,7 +29,6 @@ class AckermannControllerMock : public AckermannController{
    * @return void
    */
   MOCK_METHOD0(getTheta, double);
-  
   /*
    * @brief : The given function mocks the method computeLH 
    * 
@@ -21,16 +36,14 @@ class AckermannControllerMock : public AckermannController{
    * @return void
    */
   MOCK_METHOD1(computeLH, double);
-  
   /*
    * @brief : The given function mocks the method controlConstants 
    * 
    * @param none
    * @return void
    */
-  MOCK_METHOD2(controlConstants, std::vector<double>);  
+  MOCK_METHOD2(controlConstants, std::vector<double>);
 };
-
 /**
  * @brief Test case to test that steering angle
  * is calculated properly
@@ -39,11 +52,12 @@ class AckermannControllerMock : public AckermannController{
  */
 TEST(ackermannControllerTest, steeringTest) {
   AckermannControllerMock controllerMock;
-  ::testing::Expectation init = EXPECT_CALL(controllerMock, computeLH(_)).Times(1);
-  ::testing::Expectation init = EXPECT_CALL(controllerMock, controlConstants(_)).Times(1);
+  ::testing::Expectation init = EXPECT_CALL(controllerMock,
+                                            computeLH(_)).Times(1);
+  ::testing::Expectation init = EXPECT_CALL(controllerMock,
+                                            controlConstants(_)).Times(1);
   EXPECT_CALL(controllerMock, getTheta(_)).After(init);
-  
-  std::vector<double> testD{20.5,30.2};
+  std::vector<double> testD{20.5, 30.2};
   double testTheta = 30.0;
   mock.computeSteering(testD, testTheta);
 }
